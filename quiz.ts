e65cb97dab2a8ce5e8c12e3e0eb25d81aa724b6b
ccas09 <70821992+ccas09@users.noreply.github.com>
2020-10-21 20:14:14 -0300

    Add files via upload

---
+++ quiz.ts
@@ -0,0 +1,22 @@
+
+let  cajasRojas ;
+let cajasAzules;
+let cajasVerdes;
+
+function apretoCajaRoja(){
+let cajasRojas = document.getElementsByClassName('cajaRoja');
+
+}
+
+function apretoCajaAzul(){
+    let cajasAzules = document.getElementsByClassName('cajaAzul');
+
+    
+
+}
+
+function apretoCajaVerde(){
+    let cajasVerdes = document.getElementsByClassName('cajaVerde');
+
+    
+}
